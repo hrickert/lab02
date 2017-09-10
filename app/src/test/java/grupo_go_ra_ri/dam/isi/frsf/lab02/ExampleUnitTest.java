b28c7ad9db2894736bda0182902f12279367f48b
hrickert <hernan_hjr@hotmail.com>
2017-09-10 18:57:42 -0300

    first commit

---
+++ app/src/test/java/grupo_go_ra_ri/dam/isi/frsf/lab02/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package grupo_go_ra_ri.dam.isi.frsf.lab02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
